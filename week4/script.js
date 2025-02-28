dcae38fc2a71bf94f5b027afbda270e645455df4
MireyaZamora <mireyazamora@berkeley.edu>
2025-02-27 20:24:23 -0800

    Trying adding animation when clicked and rotating

---
+++ week4/script.js
@@ -0,0 +1,9 @@
+function addOneToCounter(){
+    document.getElementById("counter").innerText = parseInt(document.getElementById("counter").innerText) +1
+    var degrees = 0;
+    $('.img').click(function rotateMe(e){
+        dgrees+= 90
+
+
+    })
+}
